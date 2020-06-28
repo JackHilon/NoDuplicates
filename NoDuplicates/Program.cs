4ccd2fbfc3cd67dd2ce844901a648a3764f8fca7
markandmark1111@gmail.com <markandmark1111@gmail.com>
2020-06-28 19:30:37 +0200

    Add project files.

---
+++ NoDuplicates/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace NoDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // No Duplicates
+            // https://open.kattis.com/problems/nodup
+            // check replicate item in the array
+
+
+            var myStringArray = GetALine();
+            var result = StringArrayCheck(myStringArray);
+            Console.WriteLine(result);
+        }
+        private static string StringArrayCheck(string[] yourArray)
+        {
+            for (int i = 0; i < yourArray.Length-1; i++)
+            {
+                if (ItemExist(yourArray, yourArray[i], i+1) == true)
+                    return "no";
+            }
+            return "yes";
+        }
+
+        private static bool ItemExist(string[] yourArray, string str, int yourIndex)
+        {
+            for (int i = yourIndex; i < yourArray.Length; i++)
+            {
+                if (yourArray[i] == str)
+                    return true;
+            }
+            return false;
+        }
+
+        private static string[] GetALine()
+        {
+            string str = Console.ReadLine();
+            string[] arr;
+            try
+            {
+                arr = str.Split(' ');
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return GetALine();
+            }
+            return arr;
+        }
+    }
+}
